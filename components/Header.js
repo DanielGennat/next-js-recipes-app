3ab2bf45305a5fbbadd84e9dd75c08b255ad2a96
DanielGennat <daniel.gennat@freenet.de>
2022-09-06 13:02:07 +0200

    Layout

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <>
+        <h1>This is the Header</h1>
+        <p>Welcome</p>
+        </>
+    )
+}
---
+++ pages/Dessert.js
@@ -0,0 +1,10 @@
+export default function Dessert() {
+
+    return (
+        <>
+        <h2>Dessert</h2>
+        <p>This is some dummy content for the Dessert page.</p>
+        <p>This could be your favorite page!</p>
+        </>
+    )
+}
